7f4f25b81cc5aac0212b3066cbf777335d590e89
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2024-02-12 09:00:19 +0100

    some scripts

---
+++ reco.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+set -e
+
+COMPACT_FILE=$K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml
+FILE=../../E250-SetA.P4f_sw_sl.Gwhizard-2_8_5.eL.pR.I500106.0
+NEVT=10000
+
+pushd CLDConfig/CLDConfig
+
+k4run CLDReconstruction.py -n $NEVT --inputFiles $FILE.10000.SIM.edm4hep.root --outputBasename $FILE.$NEVT.REC
+
+popd
---
+++ sim.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+set -e
+
+COMPACT_FILE=$K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml
+FILE=E250-SetA.P4f_sw_sl.Gwhizard-2_8_5.eL.pR.I500106.0
+NEVT=10000
+
+ddsim --compactFile $COMPACT_FILE \
+      --outputFile $FILE.$NEVT.SIM.edm4hep.root \
+      --inputFile $FILE.slcio \
+      --steeringFile CLDConfig/CLDConfig/cld_steer.py \
+      --random.seed 0123456789 \
+      --numberOfEvents $NEVT
