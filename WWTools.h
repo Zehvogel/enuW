858e50e25bc9130686883946fc68fbf398f149fb
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2024-11-04 17:28:40 +0100

    ....

--- WWTools.h
+++ WWTools.h
@@ -5,14 +5,12 @@
 namespace WWTools
 {
     using namespace ROOT::Math;
+
+    // takes W and fermion 4 vector in the WW/CMS frame and input coordinate system specified by y and z
     template <class CoordSystem>
     LorentzVector<CoordSystem>
-    starVector(const LorentzVector<CoordSystem>& W_lvec, const LorentzVector<CoordSystem>& lep_lvec, const XYZVector& e_minus_vec = XYZVector(0, 0, 1))
+    starVectorBasis(const LorentzVector<CoordSystem>& W_lvec, const LorentzVector<CoordSystem>& lep_lvec, const XYZVector& y, const XYZVector& z)
     {
-        // z* points along the W flight direction in the lab frame
-        const auto z = W_lvec.Vect().Unit();
-        // y* points in the direction of e- x W
-        const auto y = e_minus_vec.Cross(z).Unit();
         // the obvious third dimension
         const auto x = y.Cross(z);
 
@@ -25,5 +23,55 @@ namespace WWTools
         const auto transformed_lvec = rot(boosted_lvec);
 
         return transformed_lvec;
+    }
+
+    template <class CoordSystem>
+    LorentzVector<CoordSystem>
+    starVectorHagiwara(const LorentzVector<CoordSystem>& W_lvec, const LorentzVector<CoordSystem>& lep_lvec, const XYZVector& e_minus_vec, const LorentzVector<CoordSystem>& W_minus_lvec)
+    {
+        // z* points along the W flight direction in the lab frame
+        const auto z = W_minus_lvec.Vect().Unit();
+        // y* points in the direction of e- x W
+        const auto y = e_minus_vec.Cross(z).Unit();
+
+        return starVectorBasis(W_lvec, lep_lvec, y, z);
+    }
+
+    template <class CoordSystem>
+    LorentzVector<CoordSystem>
+    starVector(const LorentzVector<CoordSystem>& W_lvec, const LorentzVector<CoordSystem>& lep_lvec, const XYZVector& e_minus_vec = XYZVector(0, 0, 1))
+    {
+        // z* points along the W flight direction in the lab frame
+        const auto z = W_lvec.Vect().Unit();
+        // y* points in the direction of e- x W
+        const auto y = e_minus_vec.Cross(z).Unit();
+
+        return starVectorBasis(W_lvec, lep_lvec, y, z);
+    }
+
+    // XXX: don't use, this is certainly wrong!
+    using namespace ROOT::Math;
+    template <class CoordSystem>
+    LorentzVector<CoordSystem>
+    starVectorOO(const LorentzVector<CoordSystem>& W_lvec, const LorentzVector<CoordSystem>& lep_lvec, const XYZVector& e_minus_vec = XYZVector(0, 0, 1))
+    {
+        // z* points along the W flight direction in the lab frame
+        // const auto z = W_lvec.Vect().Unit();
+        // y* points in the direction of e- x W
+        // const auto y = e_minus_vec.Cross(z).Unit();
+        // the obvious third dimension
+        // const auto x = y.Cross(z);
+
+        // don't rotate
+        // .Inverse() to obtain passive rotation (rotate coord system not vector)
+        // const auto rot = Rotation3D(x, y, z).Inverse();
+
+        // get the boost to boost something into the W CMS, boost and then rotate
+        const auto W_boost = W_lvec.BoostToCM();
+        const auto boosted_lvec = VectorUtil::boost(lep_lvec, W_boost);
+        // const auto transformed_lvec = rot(boosted_lvec);
+
+        // return transformed_lvec;
+        return boosted_lvec;
     }
 }
